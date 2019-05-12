834492cabff2f4edfa9504e90fd82fa75685e48f
Suyash Sonawane <suyashsonawane007@gmail.com>
2019-05-12 13:10:19 +0530

    removed keys

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 var config = {
-    apiKey: "AIzaSyDv7koS_pHn_ylXm3Ivb9tY9vg552z8t5k",
-    authDomain: "ninja-smoothies-76f74.firebaseapp.com",
-    databaseURL: "https://ninja-smoothies-76f74.firebaseio.com",
-    projectId: "ninja-smoothies-76f74",
-    storageBucket: "ninja-smoothies-76f74.appspot.com",
-    messagingSenderId: "358319472662",
-    appId: "1:358319472662:web:434c4a3455d6dff3"
+// keys here
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(config);
